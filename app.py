5ae4d2e91c0b1a449c2196b816eae123d5784e62
TobiasBoegel <boegeltobias@gmail.com>
2024-11-04 17:25:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ df = pd.read_csv('vehicles_us.csv')
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 #Trouble shooting Render errors
 import numpy as np
-df['price'] = df['price'].astype(object)
+import pyarrow as pa
+df['price'] = df['price'].astype(float)
 df['date_posted'] = pd.to_datetime(df['date_posted'])
 df['days_listed'] = df['days_listed'].astype(float)
 #Displaying DataFrame with Streamlit
